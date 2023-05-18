5051714aff9b854b1313f4a0ac4a4340079549d3
harjeeyborlah <harjeeyborlah@gmail.com>
2023-05-18 21:06:11 +0100

    evening 18/5/2023

--- app.js
+++ app.js
@@ -1,66 +1,67 @@
-  const sections = document.querySelectorAll('.section');
-  const sectBtns = document.querySelectorAll('.controls');
-  const sectBtn = document.querySelectorAll('.control');
-  const allSections = document.querySelector('.main-content');
+const sections = document.querySelectorAll(".section");
+const sectBtns = document.querySelectorAll(".controls");
+const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
 
-  function PageTransitions(){
-    //button click active class
-    for(let i = 0; i < sectBtn.length; i++){
-        sectBtn[i].addEventListener('click', function(){
-            let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn','');
-            this.className += ' active-btn';
-        })
-    }
-
-    //sections active class
-    allSections.addEventListener('click',(e)=>{
-        const id = e.target.dataset.id;
-        if(id){
-            //remove selected from the other btns
-            sectBtns.forEach((btn) =>{
-                btn.classList.remove('active')
-            })
-            e.target.classList.add('active')
-            
-
-            //hide other sections
-            sections.forEach((section) =>{
-                section.classList.remove('active')
-            })
-            const element = document.getElementById(id);
-            element.classList.add('active');
-        }
-    })
-
-    //toggle theme
-    const themeBtn = document.querySelector('.theme-btn');
-    themeBtn.addEventListener('click',()=>{
-      let element = document.body;
-      element.classList.toggle('light-mode')
-    })
+function PageTransitions() {
+  //button click active class
+  for (let i = 0; i < sectBtn.length; i++) {
+    sectBtn[i].addEventListener("click", function () {
+      let currentBtn = document.querySelectorAll(".active-btn");
+      currentBtn[0].className = currentBtn[0].className.replace(
+        "active-btn",
+        ""
+      );
+      this.className += " active-btn";
+    });
   }
 
-  PageTransitions();
+  //sections active class
+  allSections.addEventListener("click", (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      //remove selected from the other btns
+      sectBtns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active");
 
-
-
+      //hide other sections
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+      const element = document.getElementById(id);
+      element.classList.add("active");
+    }
+  });
 
   // Show the scroll-to-top button when the user scrolls down
-window.addEventListener('scroll', function() {
-  var scrollToTop = document.querySelector('.scroll-to-top');
-  if (window.pageYOffset > 100) {
-    scrollToTop.style.display = 'block';
-  } else {
-    scrollToTop.style.display = 'none';
-  }
-});
-
-// Smoothly scroll back to the top of the page when the user clicks the scroll-to-top button
-document.querySelector('.scroll-to-top a').addEventListener('click', function(e) {
-  e.preventDefault();
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
+  window.addEventListener("scroll", function () {
+    var scrollToTop = document.querySelector(".scroll-to-top");
+    if (window.pageYOffset > 100) {
+      scrollToTop.style.display = "block";
+    } else {
+      scrollToTop.style.display = "none";
+    }
   });
-});
+
+  // Smoothly scroll back to the top of the page when the user clicks the scroll-to-top button
+  document
+    .querySelector(".scroll-to-top a")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    });
+
+  //toggle theme
+  const themeBtn = document.querySelector(".theme-btn");
+  themeBtn.addEventListener("click", () => {
+    let element = document.body;
+    element.classList.toggle("light-mode");
+  });
+}
+
+PageTransitions();
